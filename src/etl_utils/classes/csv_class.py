06f55577c8ac6451f607b64a52302fba3c354818
ItaSsa <itairassa@gmail.com>
2025-03-10 13:38:51 +0100

    Including recursive filter method

--- src/etl_utils/classes/csv_class.py
+++ src/etl_utils/classes/csv_class.py
@@ -4,7 +4,7 @@ class CsvProcessor:
     def __init__(self,file_path:str):
         self.file_path = file_path
         self.df = None
-        self.df_filtered = None
+        self.df_filtered = pd.DataFrame()
     
     def load_csv(self):
         self.df = pd.read_csv(self.file_path)
@@ -16,3 +16,25 @@ class CsvProcessor:
     
     def sub_filter(self, column:str, attribute:str):
         return self.df_filtered[ self.df_filtered[column] == attribute] 
+    
+    def recursive_filter_as(self,columns:list[str], attribute:list[str]):
+
+        if len(columns) != len(attribute):
+            raise ValueError("You should inform the same number of columns and attributes")
+
+        if len(columns) == 0: 
+            return self.df
+        
+        current_column = columns[0]
+        current_attribute = attribute[0]
+        
+        if self.df_filtered.empty:
+            df_filtered = self.df[ self.df[current_column] == current_attribute]
+            self.df_filtered = df_filtered
+        else:
+            df_filtered = self.df_filtered[ self.df_filtered[current_column] == current_attribute]
+
+        if len(columns) == 1: #break condition
+            return df_filtered 
+        else:
+            return self.recursive_filter_as(columns[1:], attribute[1:])# The rest of columns
--- src/etl_utils/interface/csv_testing.py
+++ src/etl_utils/interface/csv_testing.py
@@ -1,15 +1,18 @@
 from etl_utils.classes.csv_class import CsvProcessor
 
-v_file = "src/etl_utils/data/csv_file_test.csv"
+v_file = "etl_utils/data/csv_file_test.csv"
 v_filter_column = "state"
 v_filter_attribute = 'SP'
 
 v_csv_file = CsvProcessor(v_file)
 v_csv_file.load_csv() #load csv
-# Filtering once
-v_df_filtered = v_csv_file.filter_as(v_filter_column,v_filter_attribute)
-print(v_df_filtered)
+# # Filtering once
+# v_df_filtered = v_csv_file.filter_as(v_filter_column,v_filter_attribute)
+# print(v_df_filtered)
 
-#Filtering again
-v_df_filtered = v_csv_file.sub_filter('price',5.6)
+# #Filtering again
+# v_df_filtered = v_csv_file.sub_filter('price',5.6)
+# print(v_df_filtered)
+
+v_df_filtered = v_csv_file.recursive_filter_as(['state','date'],['BA','21/01/2024'])
 print(v_df_filtered)
